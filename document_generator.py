4d6597512bd7c838d959fdd68d33ef89435dce2e
bigi <bigigoo@gmail.com>
2021-12-09 19:55:17 +0100

    Added random noise to random cahracters (per character, per font, per document)

--- document_generator.py
+++ document_generator.py
@@ -13,37 +13,8 @@ import operator
 import warnings
 from operator import itemgetter
 from perlin_numpy import generate_perlin_noise_2d
-
-
-class Augmentation:
-
-    def __init__(self):
-        self.fonts: list[PIL.ImageFont.FreeTypeFont] = []
-        self.augmentation_num: list[int] = []
-        self.characters: list[str] = []
-        self.offsets: list[list[tuple(int, int)]] = []
-        self.noise_masks: list[np.ndarray] = []
-
-    def add_font(self, font: PIL.ImageFont.FreeTypeFont, offset_range: tuple = (-3, 3),
-                 noise_octave: int = 8):
-        self.fonts.append(font)
-        self.augmentation_num.append(np.random.randint(1, 10))
-        offsets = []
-        draw = ImageDraw.Draw(Image.new('RGB', (256, 256), color=(255, 255, 255)))
-        for i in range(self.augmentation_num[-1]):
-            offsets.append((np.random.randint(offset_range[0], np.random.randint(offset_range[1]))))
-        self.offsets.append(offsets)
-        masks = []
-        for i in range(self.augmentation_num[-1]):
-            np.random.seed()
-            char_size_x, char_size_y = draw.textsize(' ', font)
-            if char_size_x % noise_octave:
-                char_size_x += noise_octave - (char_size_x % noise_octave)
-            if char_size_y % noise_octave:
-                char_size_y += noise_octave - (char_size_y % noise_octave)
-            noise = generate_perlin_noise_2d((char_size_y, char_size_x), (noise_octave, noise_octave))
-            masks.append(noise)
-        self.noise_masks.append(masks)
+import random
+import cv2
 
 
 class Object:
@@ -80,6 +51,54 @@ class Text:
         self.lines_bb = []
 
 
+class Augmentation:
+
+    def __init__(self):
+        self.fonts: list[PIL.ImageFont.FreeTypeFont] = []
+        self.augmentation_num: list[int] = []
+        self.characters: list[list[str]] = []
+        self.offsets: list[list[tuple(int, int)]] = []
+        self.noise_masks: list[np.ndarray] = []
+
+    def add_fonts(self, fonts: list[PIL.ImageFont.FreeTypeFont],
+                  texts: list[Text],
+                  offset_range: tuple = (-3, 3),
+                  noise_octave: int = 8):
+
+        for font in fonts:
+            chars: str = ''
+            for text in texts:
+                if text.font == font:
+                    chars += text.text.replace(" ", "")
+
+            self.fonts.append(font)
+            aug_num = np.random.randint(1, 10)
+            self.augmentation_num.append(aug_num)
+            offsets = []
+            draw = ImageDraw.Draw(Image.new('RGB', (256, 256), color=(255, 255, 255)))
+            for i in range(self.augmentation_num[-1]):
+                offsets.append((np.random.randint(offset_range[0], np.random.randint(offset_range[1]))))
+            self.offsets.append(offsets)
+            masks = []
+            unique_chars = list(set(chars))
+            chars_to_aug = random.sample(unique_chars, aug_num)
+            self.characters.append(chars_to_aug)
+            for character in chars_to_aug:
+                np.random.seed()
+                char_size_x, char_size_y = draw.textsize(character, font)
+                char_size_x += 10
+                char_size_y += 10
+                mask_x, mask_y = (char_size_x, char_size_y)
+                if char_size_x % noise_octave:
+                    mask_x += noise_octave - (char_size_x % noise_octave)
+                if char_size_y % noise_octave:
+                    mask_y += noise_octave - (char_size_y % noise_octave)
+                noise = generate_perlin_noise_2d((mask_y, mask_x), (noise_octave, noise_octave))
+                noise_mask = noise[0:char_size_y, 0: char_size_x]
+                masks.append(noise_mask)
+            self.noise_masks.append(masks)
+
+
 class Box:
 
     def __init__(self, size: tuple, name: str = '', background_color: tuple = (255, 255, 255)):
@@ -103,12 +122,23 @@ class Box:
         for idx, char in enumerate(text.text):
             char_size_x, char_size_y = draw.textsize(char, text.font)
             char_offset = text.font.getoffset(char)
+            im_aug = np.zeros(1)
             if char != ' ':
                 char_image = Image.new('RGB', (char_size_x + 10, char_size_y + 10), color=(255, 255, 255))
                 draw_tool = ImageDraw.Draw(char_image)
-                draw_tool.text((10, 10), char, (0, 0, 0), text.font)
+                draw_tool.text((10, 10), char, text.color, text.font)
                 coords = (np.asarray(char_image.convert('L')) < 255).nonzero()
-                coords = (coords[0] - 10, coords[1] - 10)
+                coords_glob = (coords[0] - 10, coords[1] - 10)
+                if text.font in augmentations.fonts:
+                    font_id = augmentations.fonts.index(text.font)
+                    if char in augmentations.characters[font_id]:
+                        char_id = augmentations.characters[font_id].index(char)
+                        mask = augmentations.noise_masks[font_id][char_id]
+                        char_mask = (np.asarray(char_image.convert('RGB')) == 255).astype(np.uint8) * 255
+                        char_im = np.asarray(char_image.convert('RGB'))
+                        mask = np.dstack([(mask * 255).astype(int)]*3)
+                        im_aug = cv2.add(char_im, mask, dtype=0)
+                        im_aug = cv2.bitwise_or(im_aug, char_mask)
             else:
                 char_image = Image.new('RGB', (char_size_x, char_size_y))
                 draw_tool = ImageDraw.Draw(char_image)
@@ -132,8 +162,14 @@ class Box:
                     text.words.append(word_coords)
                 break
 
-            global_coords = (indentation[0] + coords[0] + self.offset_y, indentation[1] + coords[1] + self.offset_x)
-            draw.text((indentation[1] + self.offset_x, indentation[0] + self.offset_y), char, text.color, text.font)
+            global_coords = (indentation[0] + coords_glob[0] + self.offset_y,
+                             indentation[1] + coords_glob[1] + self.offset_x)
+            if im_aug.any():
+                pixels = self.image.load()
+                for glob, loc in zip(np.asarray(global_coords).T, np.asarray(coords).T):
+                    pixels[tuple(glob[::-1])] = tuple(im_aug[tuple(loc)])
+            else:
+                draw.text((indentation[1] + self.offset_x, indentation[0] + self.offset_y), char, text.color, text.font)
             self.offset_x += char_size_x
             # print(char)
             obj = Object(value=char, mask=global_coords)
@@ -259,27 +295,47 @@ class Document:
 
 
 def main():
-
     my_doc = Document('a4')
-    augment = Augmentation()
+
+    boxes = []
+    texts = []
+    fonts = []
+
     box1 = Box((500, 500), 'box1')
+    box2 = Box((300, 300), 'box2')
+
+    boxes.append(box1)
+    boxes.append(box2)
+
     font1 = ImageFont.truetype('./comic.ttf', 32)
-    text_b1 = Text('It\'s a beatifull day.', font1, underline=1, underline_width=3,
-                   underline_offset=3)
     font2 = ImageFont.truetype('./luckytw.ttf', 25)
+    font3 = ImageFont.truetype('./LITERPLA.ttf', 32)
+
+    fonts.append(font1)
+    fonts.append(font2)
+    fonts.append(font3)
+
+    text_b1 = Text('It\'s a beatifull day.', font1, underline=1, underline_width=3,
+                   underline_offset=3, color=(255, 0, 0))
     text_b1_2 = Text('Protikorupční organizace Transparency International současně také sdělila,'
                      'že Blažek by neměl být ministrem, protože by to budilo pochybnosti o ovlivňování vyšetřování'
                      'zmíněných kauz.', font2)
-    augment.add_font(font1)
-    augment.add_font(font2)
+    text_b2 = Text(r'Мой распорядок дня.', font3, underline=True, underline_width=3)
+
+    texts.append(text_b1)
+    texts.append(text_b1_2)
+    texts.append(text_b2)
+
+    augment = Augmentation()
+    augment.add_fonts(fonts, texts)
+
     box1.add_text(text_b1, augment)
     box1.add_text(text_b1_2, augment)
+    box2.add_text(text_b2, augment)
+
     my_doc.add_box(box1, (10, 10))
-    box2 = Box((300, 300), 'box2')
-    strings = r'Мой распорядок дня.'
-    text_b2 = Text(strings, ImageFont.truetype('./LITERPLA.ttf', 32), underline=True, underline_width=3)
-    box2.add_text(text_b2)
     my_doc.add_box(box2, (10, 510))
+
     fig, ax = plt.subplots()
     ax.imshow(my_doc.image)
     bblist = my_doc.get_text_bounding_boxes()
