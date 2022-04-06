6a44ea03b26cd400c75a780af975490eca7824d6
mhlzcu <mhlavac@kky.zcu.cz>
2022-04-06 13:41:43 +0200

    test for duplicate masks for different characters

--- font_test.py
+++ font_test.py
@@ -22,32 +22,52 @@ def pil_character_test(font_path: str, test_text: str) -> (bool, bool):
     can_generate = True
     contains_empty_masks = False
     font = ImageFont.truetype(font_path, 10)
+    repeating_masks = []
+    contains_repeating_masks = False
+    processed_characters = []
     for character in test_text:
         try:
             im = Image.new("RGB", (100, 100), (255, 255, 255))
             draw = ImageDraw.Draw(im)
-            draw.text((10, 10), character, font=font)
+            draw.text((50, 50), character, (0, 0, 0), font=font)
             im_np = np.array(im)
-            if im_np.min() == 255:
+            if character not in processed_characters:
+                processed_characters.append(character)
+                repeating_masks.append((im_np, character))
+            if im_np.min() == 255 and character != ' ':
                 contains_empty_masks = True
+                print('Empty mask for ', character)
         except:
             can_generate = False
             return can_generate, contains_empty_masks
 
-    return can_generate, contains_empty_masks
+    for idx, arr in enumerate(repeating_masks):
+        for mask in repeating_masks[(idx+1):]:
+            same_val = np.array_equal(arr[0], mask[0])
+            if same_val and arr[1] != mask[1]:
+                contains_repeating_masks = True
+                if arr[1] == ' ':
+                    print('Same mask for symbols:', 'space', mask[1])
+                elif mask[1] == ' ':
+                    print('Same mask for symbols:', arr[1], 'space')
+                else:
+                    print('Same mask for symbols:', arr[1], mask[1])
+
+    return can_generate, contains_empty_masks, contains_repeating_masks
 
 
 
 def main():
-    path = 'Melon_Font_-_Free_Trial.ttf'
+    path = 'Finding_Beauty.ttf'
     test_string = 'LOĎ ČEŘÍ KÝLEM TŮŇ OBZVLÁŠŤ V GRÓNSKÉ ÚŽINĚ. \
     PŘÍLIŠ ŽLUŤOUČKÝ KŮŇ ÚPĚL ĎÁBELSKÉ KÓDY. \
     Loď čeří kýlem tůň obzvlášť v Grónské úžině. \
     Příliš žluťoučký kůň úpěl ďábelské kódy.'
     contains_test = font_character_test(path, test_string)
-    printable_test, empty_masks = pil_character_test(path, test_string)
+    printable_test, empty_masks, repeating_masks = pil_character_test(path, test_string)
     print('Font supports testing string: ', contains_test)
     print('Font contains empty masks: ', empty_masks)
+    print('Font contains repeating masks: ', repeating_masks)
     print('PIL is able to print testing string: ', printable_test)
 
 
